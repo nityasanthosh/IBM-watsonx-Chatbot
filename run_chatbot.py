8e9d1671df5c7d3a4d7ef17602378e1591c9ce60
nityasanthosh <nityasanthosh2004@gmail.com>
2025-05-10 22:07:00 +0530

    Added the chatbot code files and all the related resources

---
+++ run_chatbot.py
@@ -0,0 +1,7 @@
+import webbrowser
+import os
+
+file_path = os.path.abspath("C:\Users\Lenovo India\OneDrive\Desktop\Bistro_Cafe_Assistant\Bistro.html")
+
+# Open in the default web browser
+webbrowser.open(f"file://{file_path}")
